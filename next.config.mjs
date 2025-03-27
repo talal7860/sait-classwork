f153812aeb56484557a590122b14b02bb6835c3e
Talal Arshad <talal7860@gmail.com>
2025-03-27 13:49:52 -0600

    adds eslint

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    dirs: ['components', 'app', 'contexts', 'hooks', 'pages', 'utils'], // Only run ESLint on the 'src' and 'app' directories during production builds (next build)
+  },
+};
 
 export default nextConfig;
